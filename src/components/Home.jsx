012126de5b8f849ba022f7d3d5f8656267b7449d
domagojkuzmek <domagoj.kuzmek421@gmail.com>
2025-05-25 12:12:57 +0200

    feat: defined design tokens (colors, spacing, typography, radius)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Home() {
   return (
-    <div>
-      <h1>Blog site 1</h1>
+    <div className="p-6 bg-yellow-200">
+      <h1 className="text-preset-8">Blog site 1</h1>
     </div>
   );
 }
